98f1154308b09044e4b6ce4e52c6266e933c279c
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2023-04-22 05:11:47 +0300

    again

--- Src/Collect.php
+++ Src/Collect.php
@@ -1,1 +1,20 @@
 <?php
+namespace Collect;
+
+class Collect
+{
+    public static function set($name, $value): void
+    {
+        $_SESSION[$name] = $value;
+    }
+
+    public static function get($name)
+    {
+        return $_SESSION[$name] ?? null;
+    }
+
+    public static function clear($name)
+    {
+        unset($_SESSION[$name]);
+    }
+}
