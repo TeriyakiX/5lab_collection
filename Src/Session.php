60dbfd1d940de1d962522e43ebf266ec02d81e42
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2023-04-22 06:47:13 +0300

    update collection now

--- Src/Session.php
+++ Src/Session.php
@@ -1,5 +1,5 @@
 <?php
-namespace Collect;
+namespace Session;
 
 class Session
 {
--- Src/helpers.php
+++ Src/helpers.php
@@ -1,5 +1,5 @@
 <?php
-namespace Collect;
+namespace Session;
 
 function collectionSession(array $array = []): Session
 {
